306754918617d3dea04ee1c0a05e02f2f45aab87
surajbhadola0001 <46843145+surajbhadola01@users.noreply.github.com>
2022-03-28 10:46:04 +0530

    changed the time zone from asia to usa

--- app/Http/Controllers/Api/gameController.php
+++ app/Http/Controllers/Api/gameController.php
@@ -104,7 +104,7 @@ class gameController extends Controller
     public function getMultiPlayerGame()
     {
         try{
-            date_default_timezone_set("Asia/Calcutta");
+            date_default_timezone_set("America/New_York");
 
             $games =  game::where('published', 1)
             ->where('deleted', 0)
