b6040d9700939cb9787d5992efcada64f8235376
surajbhadola0001 <46843145+surajbhadola01@users.noreply.github.com>
2022-03-29 13:13:05 +0530

    Update gameController.php

--- app/Http/Controllers/Api/gameController.php
+++ app/Http/Controllers/Api/gameController.php
@@ -133,7 +133,7 @@ class gameController extends Controller
     
             $data = [
                 'success' => true,
-                'data' => $game_details,
+                'data' => $games_details,
                 'error' => null,
                 'status' => 200
             ];
